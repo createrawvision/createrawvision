0f29fa277e6acf88d07970ae1531d38a97ab24d2
Josef Wittmann <josefwittmann42@gmail.com>
2020-06-09 16:31:42 +0200

    Added local option to pull script

--- .scripts/pull.php
+++ .scripts/pull.php
@@ -13,7 +13,7 @@
  * - Git remote and valid SSH key for it
  * 
  * Usage (from within WP-root):
- * wp eval-file .scripts/pull.php [skip-db, skip-files]
+ * wp eval-file .scripts/pull.php [skip-db, skip-files, local]
  */
 
 
@@ -26,7 +26,6 @@ $port = 18765;
 $path = '/home/customer/www/createrawvision.de/public_html';
 
 $old_url = 'https://createrawvision.de';
-$new_url = 'http://crv.test';
 
 // folders inside wp-content (without trailing slash!)
 $folders = ['mu-plugins', 'plugins', 'uploads/2020'];
@@ -47,46 +46,48 @@ if (!defined('WP_CLI')) {
 
 $skip_db = in_array('skip-db', $args);
 $skip_files = in_array('skip-files', $args);
+$local = in_array('local', $args);
 
 
 // Avoid the output buffer
 ob_end_flush();
 ob_implicit_flush();
 
+if ($local) {
+    $pull_helper = new LocalPullHelper($path, $old_url, get_bloginfo('url'));
+} else {
+    $pull_helper = new RemotePullHelper($host, $user, $port, $path, $old_url, get_bloginfo('url'));
+}
+
+WP_CLI::log('Asserting conditions to run script');
+if (!$pull_helper->assert_conditions()) {
+    WP_CLI::error('Couldn\'t assert all conditions');
+}
+
 /*
  * PULLING DATABASE
  */
 if ($skip_db) {
-    echo 'Skipping database', PHP_EOL;
+    WP_CLI::log('Skipping database');
 } else {
-    echo 'Pulling Database', PHP_EOL;
+    WP_CLI::log('Pulling Database');
 
-    echo 'Testing SSH connection', PHP_EOL;
-    if (!valid_ssh($host, $user, $port, $path)) {
-        echo 'SHH connection failed', PHP_EOL;
-        exit(1);
-    }
+    WP_CLI::log('Importing Database');
+    if (!$pull_helper->pull_db())
+        WP_CLI::error('Pulling DB failed');
 
-    echo 'Pulling DB', PHP_EOL;
-    if (!pull_db($host, $user, $port, $path)) {
-        echo 'Pulling DB failed', PHP_EOL;
-        exit(1);
-    }
+    WP_CLI::log('Replacing URLs');
+    if (!$pull_helper->replace_url())
+        WP_CLI::error('Replacing URLs failed');
 
-    echo 'Replacing URLs', PHP_EOL;
-    if (!replace_url($old_url, $new_url)) {
-        echo 'Replacing URLs failed', PHP_EOL;
-        exit(1);
-    }
-
-    echo 'Deactivating plugins', PHP_EOL;
+    WP_CLI::log('Deactivating plugins');
     deactivate_plugins($plugins_to_deactivate);
 
-    echo 'Deactivating jetpack modules', PHP_EOL;
-    if (!remove_option('jetpack_active_modules', ['photon', 'tiled-gallery'])) {
-        echo 'Deactivating jetpack modules failed', PHP_EOL;
-        exit(1);
-    }
+    WP_CLI::log('Deactivating jetpack modules');
+    if (!$pull_helper->remove_option('jetpack_active_modules', ['photon', 'tiled-gallery']))
+        WP_CLI::error('Deactivating jetpack modules failed');
+
+    WP_CLI::success('Pulling database complete');
 }
 
 
@@ -94,94 +95,171 @@ if ($skip_db) {
  * SYNCING FILES
  */
 if ($skip_files) {
-    echo 'Skipping syncing files', PHP_EOL;
+    WP_CLI::log('Skipping syncing files');
 } else {
-    echo 'Syncing files', PHP_EOL;
-
-    echo 'Checking directories for wp-content', PHP_EOL;
-    if (!wp_content_in_local_path()) {
-        echo 'The local path does not have wp-content as a directory', PHP_EOL;
-        exit(1);
-    }
-    if (!wp_content_in_remote_path($host, $user, $port, $path)) {
-        echo 'The remote path does not have wp-content as a directory', PHP_EOL;
-        exit(1);
-    }
+    WP_CLI::log('Syncing files');
 
     foreach ($folders as $folder) {
-        echo "Pulling $folder", PHP_EOL;
-        if (!rsync($host, $user, $port, $path, $folder)) {
-            echo 'Pulling uploads failed', PHP_EOL;
-            exit(1);
-        }
+        WP_CLI::log("Pulling $folder");
+        if (!$pull_helper->sync_files($folder))
+            WP_CLI::error('Pulling uploads failed');
     }
 
-    echo 'Pulling files from GitHub', PHP_EOL;
-    if (!git_pull()) {
-        echo 'Pulling from GitHub failed', PHP_EOL;
-        exit(1);
-    }
+    WP_CLI::log('Pulling files from GitHub');
+    if (!$pull_helper->git_pull())
+        WP_CLI::error('Pulling from GitHub failed');
+
+    WP_CLI::log('Syncing files complete');
 }
 
 
-/*
- * FUNCTIONS
+/**
+ * Helper Class for pulling files and database
  */
-function valid_ssh($host, $user, $port, $path)
+abstract class PullHelper
 {
-    $command = "ssh -q -p $port $user@$host \"cd $path; exit;\"";
-    system($command, $return_var);
-    return $return_var == 0;
-}
+    /**
+     * Checks that all conditions to run methods are met
+     */
+    abstract public function assert_conditions();
 
-function wp_content_in_local_path()
-{
-    $command = "find . -maxdepth 1 -type d -name 'wp-content'";
-    exec($command, $output, $return_var);
-    return $return_var == 0 && $output;
-}
-
-function  wp_content_in_remote_path($host, $user, $port, $path)
-{
-    $command = "ssh -p $port $user@$host \"cd $path; find . -maxdepth 1 -type d -name 'wp-content';\"";
-    exec($command, $output, $return_var);
-    return $return_var == 0 && $output;
-}
-
-function rsync($host, $user, $port, $path, $folder)
-{
-    $command = "rsync -avh --progress --size-only -e 'ssh -p $port' $user@$host:$path/wp-content/$folder/ ./wp-content/$folder";
-    system($command, $return_var);
-    return $return_var == 0;
-}
-
-function git_pull()
-{
-    system("git pull", $return_var);
-    return $return_var == 0;
-}
-
-function pull_db($host, $user, $port, $path)
-{
-    $command = "ssh -p $port $user@$host \"cd $path; wp db export -\" | wp db import -";
-    system($command, $return_var);
-    return $return_var == 0;
-}
-
-function replace_url($old_url, $new_url)
-{
-    $command = "wp search-replace '$old_url' '$new_url' --recurse-objects --skip-columns=guid";
-    system($command, $return_var);
-    return $return_var == 0;
-}
-
-function remove_option($option_name, $keys_to_remove)
-{
-    $option_value = get_option($option_name);
-    if ($option_value == false) return false;
-    if (array_intersect($option_value, $keys_to_remove)) {
-        $new_option_value = array_values(array_diff($option_value, $keys_to_remove));
-        update_option($option_name, $new_option_value);
+    /**
+     * Checks if wp-content folder is in current directory
+     */
+    protected function wp_content_in_path($path = '.')
+    {
+        $command = "find '$path' -maxdepth 1 -type d -name 'wp-content'";
+        exec($command, $output, $return_var);
+        return $return_var == 0 && $output;
     }
-    return true;
+
+    /**
+     * Gets the files from the source
+     */
+    abstract public function sync_files($folder);
+
+    /**
+     * Pull files from GitHub
+     * @return boolean TRUE on success
+     */
+    public function git_pull()
+    {
+        system("git pull", $return_var);
+        return $return_var == 0;
+    }
+
+    /**
+     * Gets the database from the source
+     */
+    abstract public function pull_db();
+
+    /**
+     * Replace all URLs in database
+     * @return boolean TRUE on success
+     */
+    public function replace_url()
+    {
+        $command = "wp search-replace '{$this->old_url}' '{$this->new_url}' --recurse-objects --skip-columns=guid";
+        system($command, $return_var);
+        return $return_var == 0;
+    }
+
+    /**
+     * Removes entries in an option by keys
+     */
+    public function remove_option($option_name, $keys_to_remove)
+    {
+        $option_value = get_option($option_name);
+        if ($option_value == false) return false;
+        if (array_intersect($option_value, $keys_to_remove)) {
+            $new_option_value = array_values(array_diff($option_value, $keys_to_remove));
+            update_option($option_name, $new_option_value);
+        }
+        return true;
+    }
+}
+
+/**
+ * Helper Class for pulling from remote source
+ */
+class RemotePullHelper extends PullHelper
+{
+    public function __construct($host, $user, $port, $path, $old_url, $new_url)
+    {
+        $this->host = $host;
+        $this->user = $user;
+        $this->port = $port;
+        $this->path = $path;
+        $this->old_url = $old_url;
+        $this->new_url = $new_url;
+    }
+
+    public function assert_conditions()
+    {
+        return $this->valid_ssh()
+            && $this->wp_content_in_path()
+            && $this->wp_content_in_source_path();
+    }
+
+    private function valid_ssh()
+    {
+        $command = "ssh -q -p {$this->port} {$this->user}@{$this->host} \"cd {$this->path}; exit;\"";
+        system($command, $return_var);
+        return $return_var == 0;
+    }
+
+    private function wp_content_in_source_path()
+    {
+        $command = "ssh -p {$this->port} {$this->user}@{$this->host} \"cd {$this->path}; find . -maxdepth 1 -type d -name 'wp-content';\"";
+        exec($command, $output, $return_var);
+        return $return_var == 0 && $output;
+    }
+
+    public function sync_files($folder)
+    {
+        $command = "rsync -avh --progress -e 'ssh -p {$this->port}' {$this->user}@{$this->host}:{$this->path}/wp-content/$folder/ ./wp-content/$folder";
+        system($command, $return_var);
+        return $return_var == 0;
+    }
+
+    public function pull_db()
+    {
+        $command = "ssh -p {$this->port} {$this->user}@{$this->host} \"cd {$this->path}; wp db export -\" | wp db import -";
+        system($command, $return_var);
+        return $return_var == 0;
+    }
+}
+
+
+/**
+ * Helper class for pulling from local source
+ */
+class LocalPullHelper extends PullHelper
+{
+    public function __construct($path, $old_url, $new_url)
+    {
+        $this->path = $path;
+        $this->old_url = $old_url;
+        $this->new_url = $new_url;
+    }
+
+    public function assert_conditions()
+    {
+        return $this->wp_content_in_path()
+            && $this->wp_content_in_path($this->path);
+    }
+
+    public function sync_files($folder)
+    {
+        $command = "rsync -avh --progress {$this->path}/wp-content/$folder/ ./wp-content/$folder";
+        system($command, $return_var);
+        return $return_var == 0;
+    }
+
+    public function pull_db()
+    {
+        $command = "wp db export --path={$this->path} - | wp db import -";
+        system($command, $return_var);
+        return $return_var == 0;
+    }
 }
