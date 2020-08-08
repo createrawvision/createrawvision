a40141fefe1bf04f38c269623b88e18a684639af
Josef Wittmann <josefwittmann42@gmail.com>
2020-08-08 13:27:49 +0200

    Moved menu deployment last

--- .scripts/versions/deploy-0.1.0-alpha.04.php
+++ .scripts/versions/deploy-0.1.0-alpha.04.php
@@ -4,107 +4,52 @@ require_once __DIR__ . '/../wp-cli-utils.php';
 
 // Return the function to deploy all changes. Don't do anything.
 return function () {
-	deploy_breadcrumbs();
-	deploy_faqs();
+	deploy_pages_for_templates();
 };
 
 
 /**
- * Activate breadcrumbs for posts and archives
+ * Creates empty pages for template files to use
+ *
+ * Pages:
+ * * dashboard
+ * * login
  */
-function deploy_breadcrumbs() {
-	WP_CLI::log( 'Activating breadcrumbs' );
+function deploy_pages_for_templates() {
+	WP_CLI::log( 'Creating dashboard page' );
 
-	genesis_update_settings(
+	wp_insert_post(
 		array(
-			'breadcrumb_single'     => 1,
-			'breadcrumb_page'       => 0,
-			'breadcrumb_404'        => 0,
-			'breadcrumb_attachment' => 0,
-			'breadcrumb_home'       => 0,
-			'breadcrumb_front_page' => 0,
-			'breadcrumb_posts_page' => 0,
-			'breadcrumb_archive'    => 1,
+			'post_content' => '',
+			'post_title'   => 'Member Dashboard',
+			'post_name'    => 'dashboard',
+			'post_status'  => 'publish',
+			'post_type'    => 'page',
+		)
+	);
+
+	WP_CLI::log( 'Creating login page' );
+
+	wp_insert_post(
+		array(
+			'post_content' => '[login_form redirect="/dashboard"]',
+			'post_title'   => 'Login',
+			'post_name'    => 'login',
+			'post_status'  => 'publish',
+			'post_type'    => 'page',
+		)
+	);
+
+	WP_CLI::log( 'Creating search page' );
+
+	wp_insert_post(
+		array(
+			'post_content' => '',
+			'post_title'   => 'Erweiterte Rezept-Suche',
+			'post_name'    => 'suche',
+			'post_status'  => 'publish',
+			'post_type'    => 'page',
 		)
 	);
 }
 
-
-/**
- * Create FAQs page
- * Create all FAQs from /deployment_data/faqs.json
- */
-function deploy_faqs() {
-	WP_CLI::log( 'Creating FAQs page, if not already existing' );
-	$faqs_title = 'Häufig gestellte Fragen';
-	$faqs_name  = 'faqs';
-	if ( ! get_posts(
-		array(
-			'name'        => $faqs_name,
-			'post_type'   => 'page',
-			'post_status' => 'publish',
-			'numberposts' => 1,
-		)
-	) ) {
-		wp_insert_post(
-			array(
-				'post_title'  => $faqs_title,
-				'post_name'   => $faqs_name,
-				'post_type'   => 'page',
-				'post_status' => 'publish',
-			)
-		);
-	}
-
-	// Create FAQs from JSON file
-	$faqs_json     = file_get_contents( ABSPATH . '../deployment_data/faqs.json' );
-	$category_objs = json_decode( $faqs_json );
-
-	$category_progressbar = \WP_CLI\Utils\make_progress_bar( 'Creating all FAQ categories', count( $category_objs ) );
-
-	foreach ( $category_objs as $category_obj ) {
-		$category = $category_obj->category;
-
-		// Create the category term
-		$term = wp_insert_term(
-			$category->name,
-			'faq_category',
-			array( 'slug' => $category->slug )
-		);
-		if ( is_wp_error( $term ) ) {
-			WP_CLI::warning( $term->get_error_message() );
-			continue;
-		}
-		list( 'term_id' => $term_id ) = $term;
-
-		// Create all faqs
-		$faqs = $category_obj->faqs;
-
-		$items_progressbar = \WP_CLI\Utils\make_progress_bar( "Creating all FAQs in category {$category_obj->name}", count( $faqs ) );
-
-		foreach ( $faqs as $faq ) {
-			wp_insert_post(
-				array(
-					'post_title'   => $faq->title,
-					'post_content' => $faq->content,
-					'post_status'  => 'publish',
-					'post_type'    => 'faq',
-					'tax_input'    => array( 'faq_category' => array( $term_id ) ),
-				)
-			);
-
-			$items_progressbar->tick();
-		}
-
-		$items_progressbar->finish();
-		$category_progressbar->tick();
-	}
-	$category_progressbar->finish();
-
-	// Create Category to collect uncategorized FAQs
-	wp_insert_term(
-		'Sonstige Fragen',
-		'faq_category',
-		array( 'slug' => 'other-faqs' )
-	);
-}
--- .scripts/versions/deploy-0.1.0-alpha.06.php
+++ .scripts/versions/deploy-0.1.0-alpha.06.php
@@ -4,52 +4,53 @@ require_once __DIR__ . '/../wp-cli-utils.php';
 
 // Return the function to deploy all changes. Don't do anything.
 return function () {
-	deploy_pages_for_templates();
+	deploy_homepage_and_postlist();
 };
 
-
 /**
- * Creates empty pages for template files to use
- *
- * Pages:
- * * dashboard
- * * login
+ * Publish homepage and move current home (postlist) to another page
  */
-function deploy_pages_for_templates() {
-	WP_CLI::log( 'Creating dashboard page' );
-
-	wp_insert_post(
+function deploy_homepage_and_postlist() {
+	WP_CLI::log( 'Creating homepage' );
+	$homepage_content = file_get_contents( ABSPATH . '../deployment_data/homepage.html' );
+	if ( ! $homepage_content ) {
+		WP_CLI::error( 'Failed to read homepage content' );
+	}
+	$homepage_id = wp_insert_post(
 		array(
+			'post_title'   => 'Startseite',
+			'post_status'  => 'publish',
+			'post_type'    => 'page',
+			'post_content' => $homepage_content,
+		)
+	);
+	if ( ! $homepage_id ) {
+		WP_CLI::error( 'Failed to create homepage' );
+	}
+
+	WP_CLI::log( 'Creating postlist' );
+	$postlist_id = wp_insert_post(
+		array(
+			'post_title'   => 'Alle Beiträge',
+			'post_status'  => 'publish',
+			'post_type'    => 'page',
 			'post_content' => '',
-			'post_title'   => 'Member Dashboard',
-			'post_name'    => 'dashboard',
-			'post_status'  => 'publish',
-			'post_type'    => 'page',
 		)
 	);
+	if ( ! $postlist_id ) {
+		WP_CLI::error( 'Failed to create postlist' );
+	}
 
-	WP_CLI::log( 'Creating login page' );
-
-	wp_insert_post(
-		array(
-			'post_content' => '[login_form redirect="/dashboard"]',
-			'post_title'   => 'Login',
-			'post_name'    => 'login',
-			'post_status'  => 'publish',
-			'post_type'    => 'page',
-		)
+	WP_CLI::log( 'Setting homepage and blog' );
+	$options = array(
+		array( 'show_on_front', 'page' ),
+		array( 'page_on_front', $homepage_id ),
+		array( 'page_for_posts', $postlist_id ),
 	);
-
-	WP_CLI::log( 'Creating search page' );
-
-	wp_insert_post(
-		array(
-			'post_content' => '',
-			'post_title'   => 'Erweiterte Rezept-Suche',
-			'post_name'    => 'suche',
-			'post_status'  => 'publish',
-			'post_type'    => 'page',
-		)
-	);
+	foreach ( $options as list($key, $value) ) {
+		update_option( $key, $value );
+		if ( get_option( $key ) != $value ) {
+			WP_CLI::error( "Failed to set option $key to $value" );
+		}
+	}
 }
-
