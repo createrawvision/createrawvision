c2e4f57670a08ec6ab0e073d692ebea89959b662
Josef Wittmann <josefwittmann42@gmail.com>
2020-08-08 21:21:29 +0200

    Show blog list for restricted users

--- public_html/wp-content/themes/daily-dish-pro/functions.php
+++ public_html/wp-content/themes/daily-dish-pro/functions.php
@@ -948,3 +948,16 @@ add_action(
 		}
 	}
 );
+
+
+/**
+ * For now, show the normal blog home for restricted people.
+ *
+ * @todo remove when home is done
+ */
+add_filter(
+	'pre_option_show_on_front',
+	function() {
+		return crv_user_is_unrestricted() ? 'page' : 'posts';
+	}
+);
