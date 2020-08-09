4eff449ad7814063e01bf2600bc71bd7dca0f5d3
Josef Wittmann <josefwittmann42@gmail.com>
2020-08-09 12:28:49 +0200

    Help popup target=_blank

--- public_html/wp-content/themes/daily-dish-pro/lib/help-popup.php
+++ public_html/wp-content/themes/daily-dish-pro/lib/help-popup.php
@@ -21,14 +21,14 @@ function show_markup() {
 		<div class="crv-help-popup__modal">
 			<h3 class="crv-help-popup__title">Brauchst du Hilfe?</h3>
 			<p class="crv-help-popup__text">
-				Schau dir bitte zuerst unsere <a href="<?php the_permalink( get_page_by_path( 'faqs' ) ); ?>">häufig gestellten Fragen</a> an.<br>
+				Schau dir bitte zuerst unsere <a href="<?php the_permalink( get_page_by_path( 'faqs' ) ); ?>" target="_blank">häufig gestellten Fragen</a> an.<br>
 				<!-- @todo Link to introduction. -->
-				Oder versuche deine Antwort in <a href="<?php the_permalink( get_page_by_path( '' ) ); ?>">unserer Einführung</a> zu finden.<br>
+				Oder versuche deine Antwort in <a href="<?php the_permalink( get_page_by_path( 'einfuehrung' ) ); ?>" target="_blank">unserer Einführung</a> zu finden.<br>
 				Das geht meistens schneller, als auf eine Antwort zu warten.
 			</p>
 			<p class="crv-help-popup__text">
 				Falls du immer noch keine Antwort hast, kannst du uns gerne 
-				<a href="<?php the_permalink( get_page_by_path( 'kontaktformular' ) ); ?>">hier persönlich fragen</a>.
+				<a href="<?php the_permalink( get_page_by_path( 'kontaktformular' ) ); ?>" target="_blank">hier persönlich fragen</a>.
 			</p>
 		</div>
 	</section>
