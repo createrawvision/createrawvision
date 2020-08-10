02fdf4a31178543c5d07242c7fc18d8ed32bdb21
Josef Wittmann <josefwittmann42@gmail.com>
2020-08-10 06:17:58 +0200

    Dashboard styling

--- public_html/wp-content/themes/daily-dish-pro/page-dashboard.php
+++ public_html/wp-content/themes/daily-dish-pro/page-dashboard.php
@@ -4,46 +4,75 @@ namespace crv\dashboard;
 
 add_action( 'genesis_entry_content', __NAMESPACE__ . '\show_dashboard' );
 
+add_action(
+	'wp_enqueue_scripts',
+	function() {
+		wp_enqueue_style( 'daily-dish-dashboard-style', CHILD_URL . '/style-dashboard.css', array(), CHILD_THEME_VERSION );
+	}
+);
+
 function show_dashboard() {
-	echo rcp_restricted_message_pending_verification( '' );
-	echo '<div class="dashboard-container">';
+	echo wp_kses_post( rcp_restricted_message_pending_verification( '' ) );
+	echo '<div class="dashboard-container full-width">';
 
 	$sections = array(
-		'show_overview',
-		'show_recipes',
-		'show_tutorials',
-		'show_course',
-		'show_community',
-		'show_blog',
-		'show_further',
-		'show_settings',
-		'show_support',
+		'overview',
+		'recipes',
+		'course',
+		'tutorials',
+		// 'community',
+		'blog',
+		'settings',
+		'support',
+		'further',
 	);
 
 	foreach ( $sections as $section ) {
-		echo '<section>';
-		call_user_func( __NAMESPACE__ . '\\' . $section );
-		echo '</section>';
+		echo '<div class="section-wrapper"><section class="' . esc_attr( $section ) . '">';
+		call_user_func( __NAMESPACE__ . '\\show_' . $section );
+		echo '</section></div>';
 	}
 
 	echo '</div>';
 }
 
 function show_overview() {
-	echo '<h2><a href="' . esc_url( get_category_link( 4269 ) ) . '">Mitgliederbereich-Übersicht</a></h2>';
+	echo '<h2>Mitgliederbereich-Übersicht</h2>';
+	echo '<ul><li><a href="' . esc_url( get_category_link( 4269 ) ) . '"><button>Zur Übersicht</button></a></li></ul>';
+
 }
 
 function show_recipes() {
+	echo '<h2>Rohkost Rezepte</h2>';
+	echo '<div class="recipes__container">';
+	// show_recent_recipes();
 	?>
-	<h2>Rohkost Rezepte</h2>
-	<ul>
-		<li><a href="<?php echo esc_url( get_term_link( 'rezepte', 'category' ) ); ?>">Rezepte nach Kategorien</a></li>
-		<li><a href="<?php echo esc_url( get_permalink( get_page_by_path( 'suche' ) ) ); ?>">Rezepte suchen</a></li>
-		<li><a href="<?php echo esc_url( get_permalink( get_page_by_path( 'neue-rezepte' ) ) ); ?>">Neue Rezepte</a></li>
-		<li><a href="<?php echo esc_url( get_permalink( get_page_by_path( 'lesezeichen' ) ) ); ?>">Deine Lieblingsrezepte</a></li>
-		<li><a href="<?php echo esc_url( get_permalink( get_page_by_path( 'beliebte-rezepte' ) ) ); ?>">Beliebte Rezepte</a></li>
+	<ul class="recipes__list">
+		<li><a href="<?php echo esc_url( get_term_link( 'rezepte', 'category' ) ); ?>"><button>Rezepte nach Kategorien</button></a><span class="action__hint">Eine Übersicht über alle Rezepte</span></li>
+		<li><a href="<?php echo esc_url( get_permalink( get_page_by_path( 'neue-rezepte' ) ) ); ?>"><button>Neue Rezepte</button></a><span class="action__hint">Eine Rezeptübersicht von neu nach alt</span></li>
+		<li><a href="<?php echo esc_url( get_permalink( get_page_by_path( 'suche' ) ) ); ?>"><button>Rezepte suchen</button></a><span class="action__hint">Suche Rezepte nach Schwieigkeit, Typ, Ausstattung,&nbsp;...</span></li>
+		<li><a href="<?php echo esc_url( get_permalink( get_page_by_path( 'lesezeichen' ) ) ); ?>"><button>Deine Lieblingsrezepte</button></a><span class="action__hint">Deine gespeicherten Rezepte</span></li>
+		<li><a href="<?php echo esc_url( get_permalink( get_page_by_path( 'beliebte-rezepte' ) ) ); ?>"><button>Beliebte Rezepte</button></a><span class="action__hint">Rezepte, die andere oft gespeichert haben</span></li>
 	</ul>
 	<?php
+	echo '</div>';
+}
+
+function show_recent_recipes() {
+	$recipe_cat_id  = 5869;
+	$recent_recipes = get_posts(
+		array(
+			'cat'          => $recipe_cat_id,
+			'numberposts'  => 4,
+			'meta_key'     => '_thumbnail_id',
+			'meta_compare' => 'EXISTS',
+		)
+	);
+	echo '<div class="recipes__recent"><a href="' . esc_url( get_permalink( get_page_by_path( 'neue-rezepte' ) ) ) . '"><span>Neue Rezepte</span></a>';
+	foreach ( $recent_recipes as $post ) {
+		echo '<div class="recipes__overlay">' . get_the_post_thumbnail( $post, 'thumbnail-portrait', array( 'class' => 'recipes__image' ) ) . '</div>';
+	}
+	echo '</div>';
 }
 
 function show_community() {
@@ -55,11 +84,11 @@ function show_tutorials() {
 	$tutorials_category_id = get_category_by_slug( 'tipps-tutorials' )->term_id ?? 5287;
 	$courses               = get_categories( array( 'parent' => $tutorials_category_id ) );
 	?>
-	<h2><a href="<?php echo esc_url( get_category_link( $tutorials_category_id ) ); ?>">Rohkost Tipps & Tutorials</a></h2>
+	<h2>Rohkost Tipps & Tutorials</h2>
 	<ul>
 	<?php
 	foreach ( $courses as $course ) {
-		echo '<li><a href="' . esc_url( get_category_link( $course ) ) . '">' . esc_html( $course->name ) . '</a></li>';
+		echo '<li><a href="' . esc_url( get_category_link( $course ) ) . '"><button>' . esc_html( $course->name ) . '</button></a></li>';
 	}
 	?>
 	</ul>
@@ -67,18 +96,19 @@ function show_tutorials() {
 }
 
 function show_course() {
-	echo '<section><h2><a href="' . esc_url( get_category_link( 5792 ) ) . '">Dein Weg Zur Rohkost Leicht Gemacht - Kurs</a></h2></section>';
+	echo '<h2>Dein Weg Zur Rohkost Leicht Gemacht</h2>';
+	echo '<ul><li><a href="' . esc_url( get_category_link( 5792 ) ) . '"><button>Zum Kurs</button></a></li></ul>';
 }
 
 function show_blog() {
 	$blog_category_id = get_category_by_slug( 'blog' )->term_id;
 	$categories       = get_categories( array( 'parent' => $blog_category_id ) );
 	?>
-	<h2><a href="<?php echo esc_url( get_category_link( $blog_category_id ) ); ?>">Blog</a></h2>
+	<h2>Blog</h2>
 	<ul>
 	<?php
 	foreach ( $categories as $category ) {
-		echo '<li><a href="' . esc_url( get_category_link( $category ) ) . '">' . esc_html( $category->name ) . '</a></li>';
+		echo '<li><a href="' . esc_url( get_category_link( $category ) ) . '"><button>' . esc_html( $category->name ) . '</button></a></li>';
 	}
 	?>
 	</ul>
@@ -90,10 +120,10 @@ function show_further() {
 	<h2>Weiteres</h2>
 	<ul>
 		<!-- @todo -->
-		<li>Q&As</li>
+		<!-- <li><button>Q&As</button></li> -->
 		<!-- @todo -->
-		<li>Kommende Veranstaltungen</li>
-		<li><a href="<?php echo esc_url( get_permalink( get_page_by_path( 'unsere-vision' ) ) ); ?>">Unsere Vision</a></li>
+		<!-- <li><button>Kommende Veranstaltungen</button></li> -->
+		<li><a href="<?php echo esc_url( get_permalink( get_page_by_path( 'unsere-vision' ) ) ); ?>"><button>Unsere Vision</button></a></li>
 	</ul>
 	<?php
 }
@@ -103,8 +133,8 @@ function show_settings() {
 	?>
 	<h2>Einstellungen</h2>
 	<ul>
-		<li><a href="<?php the_permalink( $rcp_options['edit_profile'] ); ?>">Profil bearbeiten</a></li>
-		<li><a href="<?php the_permalink( $rcp_options['account_page'] ); ?>">Mitgliedschaft verwalten</a></li>
+		<li><a href="<?php the_permalink( $rcp_options['edit_profile'] ); ?>"><button>Profil bearbeiten</button></a></li>
+		<li><a href="<?php the_permalink( $rcp_options['account_page'] ); ?>"><button>Mitgliedschaft/Zahlungen verwalten</button></a></li>
 	</ul>
 	<?php
 }
@@ -113,9 +143,9 @@ function show_support() {
 	?>
 	<h2>Support</h2>
 	<ul>
-		<li><a href="<?php echo esc_url( get_permalink( get_page_by_path( 'faqs' ) ) ); ?>">Häufige Fragen</a></li>
-		<li><a href="<?php echo esc_url( get_permalink( get_page_by_path( 'kontaktformular' ) ) ); ?>">Kontakt</a></li>
-		<li><a href="#header-search-wrap" class="toggle-header-search">Website durchsuchen</a></li>
+		<li><a href="<?php echo esc_url( get_permalink( get_page_by_path( 'faqs' ) ) ); ?>"><button>Häufige Fragen</button></a></li>
+		<li><a href="<?php echo esc_url( get_permalink( get_page_by_path( 'kontaktformular' ) ) ); ?>"><button>Kontakt</button></a></li>
+		<li><a href="#header-search-wrap" class="toggle-header-search"><button>Website durchsuchen</button></a></li>
 	</ul>
 	<?php
 }
