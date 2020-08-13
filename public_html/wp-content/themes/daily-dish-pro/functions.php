43dd354753e074b7b670d7f3b52ad705c87cea05
Josef Wittmann <josefwittmann42@gmail.com>
2020-08-13 07:38:12 +0200

    Dirty version number change

--- public_html/wp-content/themes/daily-dish-pro/functions.php
+++ public_html/wp-content/themes/daily-dish-pro/functions.php
@@ -48,7 +48,8 @@ require_once CHILD_DIR . '/lib/woocommerce/woocommerce-notice.php';
 // Child theme (do not remove).
 define( 'CHILD_THEME_NAME', __( 'Daily Dish Pro', 'daily-dish-pro' ) );
 define( 'CHILD_THEME_URL', 'https://my.studiopress.com/themes/daily-dish/' );
-define( 'CHILD_THEME_VERSION', '2.0.0' );
+// define( 'CHILD_THEME_VERSION', '2.0.0' );
+define( 'CHILD_THEME_VERSION', '0.1.0' );
 
 add_action( 'wp_enqueue_scripts', 'daily_dish_enqueue_scripts_styles' );
 /**
