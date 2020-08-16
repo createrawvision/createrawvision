35bb649bbbd5439732abb4869fb843ac72cba965
Josef Wittmann <josefwittmann42@gmail.com>
2020-08-16 21:18:51 +0200

    Add recipe filter UX improvements

--- public_html/wp-content/themes/daily-dish-pro/lib/recipe-filter.php
+++ public_html/wp-content/themes/daily-dish-pro/lib/recipe-filter.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * Functions for the advanced recipe filter (for displaying and request handling).
+ */
 
 /**
  * Return the form for filtering recipe posts
@@ -6,13 +9,14 @@
 function crv_recipe_filter_form() {
 	ob_start(); ?>
 	<form action="<?php echo esc_url( admin_url( 'admin-ajax.php' ) ); ?>" method="POST" id="recipe-filter">
-	<input type="text" name="search" id="search" placeholder="Suchbegriff eingeben...">
-	<?php
-	crv_show_taxonomy_dropdown( 'wprm_difficulty', 'Schwierigkeitsgrad auswählen...' );
-	crv_show_taxonomy_dropdown( 'wprm_course', 'Gang/Typ auswählen...' );
-	crv_show_taxonomy_dropdown( 'wprm_cuisine', 'Küche auswählen...' );
-	crv_show_taxonomy_dropdown( 'wprm_equipment', 'Ausstattung auswählen...' );
-	?>
+		<label for="search">Suchbegriff eingeben...
+			<input type="text" name="search" id="search" placeholder="z. B. Zutat, Ausstattung oder Zubereitungsschritt">
+		</label>
+		<?php
+		crv_show_taxonomy_dropdown( 'wprm_difficulty', 'Schwierigkeitsgrad auswählen...', 'Alle Schwierigkeitsgrade' );
+		crv_show_taxonomy_dropdown( 'wprm_course', 'Gang/Typ auswählen...', 'Alle Gänge/Typen' );
+		crv_show_taxonomy_dropdown( 'wprm_cuisine', 'Küche auswählen...', 'Alle Küchen' );
+		?>
 
 		<fieldset>
 			<legend>Nach Datum sortieren</legend>
@@ -20,7 +24,7 @@ function crv_recipe_filter_form() {
 			<label><input type="radio" name="date" value="ASC" /> Älteste zuerst</label>
 		</fieldset>
 
-		<label><input type="checkbox" name="free" /> Nur kostenfreie Rezepte anezeigen</label>
+		<label><input type="checkbox" name="free"> Nur kostenfreie Rezepte anzeigen</label>
 
 		<button>Rezepte filtern</button>
 		<input type="hidden" name="action" value="crv_post_filter">
@@ -34,16 +38,21 @@ function crv_recipe_filter_form() {
  * Show select element for taxonomy
  * name: `taxonomyfilter_{$taxonomy_name}`
  */
-function crv_show_taxonomy_dropdown( $taxonomy_name, $message ) {
+function crv_show_taxonomy_dropdown( $taxonomy_name, $message_label, $message_option ) {
+	$name = 'taxonomyfilter_' . $taxonomy_name;
+	echo '<label for="' . esc_attr( $name ) . '">' . esc_html( $message_label );
+
 	wp_dropdown_categories(
 		array(
 			'hierarchical'    => true,
 			'orderby'         => 'name',
 			'taxonomy'        => $taxonomy_name,
-			'name'            => 'taxonomyfilter_' . $taxonomy_name,
-			'show_option_all' => $message,
+			'name'            => $name,
+			'show_option_all' => $message_option,
 		)
 	);
+
+	echo '</label>';
 }
 
 add_action( 'wp_ajax_crv_post_filter', 'crv_filter_recipes' );
@@ -108,24 +117,24 @@ function crv_filter_recipes() {
 			$link  = get_permalink( $post_id );
 			$title = get_the_title( $post_id );
 			?>
-	<article class="entry">
-		<header class="entry-header">
-			<h2 class="entry-title">
-			<a class="entry-title-link" href="<?php echo esc_url( $link ); ?>">
-				<?php echo esc_html( $title ); ?>
-			</a>
-			</h2>
-		</header>
-		<div class="entry-content">
+		<article class="entry">
+			<header class="entry-header">
+				<h2 class="entry-title">
+					<a class="entry-title-link" href="<?php echo esc_url( $link ); ?>">
+						<?php echo esc_html( $title ); ?>
+					</a>
+				</h2>
+			</header>
+			<div class="entry-content">
 				<?php if ( has_post_thumbnail( $post_id ) ) : ?>
-			<a class="entry-image-link" href="<?php echo esc_url( $link ); ?>">
-					<?php echo get_the_post_thumbnail( $post_id ); ?>
-			</a>
-			<?php else : ?>
-			<p>Kein Vorschaubild vorhanden...</p>
-			<?php endif; ?>
-		</div>
-	</article>
+					<a class="entry-image-link" href="<?php echo esc_url( $link ); ?>">
+						<?php echo get_the_post_thumbnail( $post_id ); ?>
+					</a>
+				<?php else : ?>
+					<p>Kein Vorschaubild vorhanden...</p>
+				<?php endif; ?>
+			</div>
+		</article>
 			<?php
 		},
 		$post_ids
