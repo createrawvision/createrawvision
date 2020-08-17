18f74ac3b96c98b853348f3b11406d7102bb310b
Josef Wittmann <josefwittmann42@gmail.com>
2020-08-17 20:15:02 +0200

    Add a recipe wish box to the dashboard

--- public_html/wp-content/themes/daily-dish-pro/page-dashboard.php
+++ public_html/wp-content/themes/daily-dish-pro/page-dashboard.php
@@ -13,7 +13,8 @@ add_action( 'genesis_entry_content', __NAMESPACE__ . '\show_dashboard', 9 );
 add_action(
 	'wp_enqueue_scripts',
 	function() {
-		wp_enqueue_style( 'daily-dish-dashboard-style', CHILD_URL . '/style-dashboard.css', array(), CHILD_THEME_VERSION );
+		wp_enqueue_style( 'crv-dashboard-style', CHILD_URL . '/style-dashboard.css', array(), CHILD_THEME_VERSION );
+		wp_enqueue_script( 'crv-reciperequest', CHILD_URL . '/js/reciperequest.js', array( 'jquery' ), CHILD_THEME_VERSION, true );
 	}
 );
 
@@ -69,8 +70,9 @@ function show_dashboard() {
 	$sections = array(
 		'overview',
 		'recipes',
-		'support',
+		'reciperequest',
 		'further',
+		'support',
 		'settings',
 	);
 
@@ -150,10 +152,53 @@ function show_recipes() {
 }
 
 /**
- * Show things, that don't fit somewhere else.
- *
- * @todo Q&As and events
+ * Shows the recipe wish box.
  */
+function show_reciperequest() {
+	?>
+	<h2 class="reciperequest__heading">Rezept&shy;wunschbox</h2>
+	<p>
+		Vermisst du eines deiner Lieblingsrezepte in Rohkost-Variante in unserem Mitgliederbereich? 
+		Dann kannst du hier deinen Wunsch abschicken. 
+		Wir bemühen uns, dein Rezept in eine roh-vegane Variante umzuwandeln.
+	</p>
+	<form class="reciperequest__box" method="POST" action="<?php echo esc_url( rest_url( 'supportcandy/v1/tickets/addRegisteredUserTicket' ) ); ?>">
+		<input name="ticket_subject" class="reciperequest__name" type="text" placeholder="Rezepttitel" aria-label="Der Titel des gewünschten Rezepts">
+		<textarea name="ticket_description" class="reciperequest__textarea"  cols="30" rows="5" placeholder="Kommentar" aria-label="Ein Kommentar zu deinem Rezeptwunschs"></textarea>
+		<button class="reciperequest_button" type="submit">Wunsch abschicken</button>
+		<input type="hidden" name="ticket_category" value="6192">
+		<?php wp_nonce_field( 'wp_rest' ); ?>
+		<div class="reciperequest__waiting reciperequest__modal">
+			Dein Wunsch wird versendet...
+		</div>
+		<div class="reciperequest__failed reciperequest__modal">
+			<p class="reciperequest__modal__heading">Etwas ging schief...</p>
+			<p>Stell sicher, dass du angemeldet bist.</p>
+			<span class="reciperequest__back">Zurück</span>
+		</div>
+		<div class="reciperequest__success reciperequest__modal">
+			<p class="reciperequest__modal__heading">Danke!</p>
+			<p>
+				<?php
+				if ( is_user_logged_in() ) {
+					echo 'Lieber ' . esc_html( get_userdata( get_current_user_id() )->first_name ) . ',<br>';
+				}
+				?>
+				Wir haben deinen Wunsch erhalten und werden dir so schnell wie möglich Bescheid geben, 
+				ob es für uns möglich ist, dein Wunschrezept in Rohkostqualität nachzumachen.
+				Je nach Wunschaufkommen, kann das aber einige Zeit dauern.
+			</p>
+			<span class="reciperequest__back">Zurück</span>
+		</div>
+	</form>
+					<?php
+}
+
+				/**
+				 * Show things, that don't fit somewhere else.
+				 *
+				 * @todo Q&As and events
+				 */
 function show_further() {
 	?>
 	<ul class="dashboard-cards">
@@ -168,12 +213,25 @@ function show_further() {
 			<a href="<?php the_permalink( get_page_by_path( 'unsere-vision' ) ); ?>" class="dashboard-cards__link">Zu unserer Vision</a>
 		</li>
 	</ul>
-	<?php
+					<?php
 }
 
-/**
- * Show all settings for the user.
- */
+				/**
+				 * Show support related items.
+				 */
+function show_support() {
+	?>
+	<h2 class="support__heading">Hilfe & Support</h2>
+	<ul class="support__list">
+		<li class="support__item"><a href="<?php echo esc_url( get_permalink( get_page_by_path( 'faqs' ) ) ); ?>">Zu den häufigen Fragen</a></li>
+		<li class="support__item"><a href="<?php echo esc_url( get_permalink( get_page_by_path( 'kontaktformular' ) ) ); ?>">Uns jetzt kontaktieren</a></li>
+	</ul>
+					<?php
+}
+
+				/**
+				 * Show all settings for the user.
+				 */
 function show_settings() {
 	global $rcp_options;
 	?>
@@ -190,21 +248,8 @@ function show_settings() {
 			<a href="<?php the_permalink( $rcp_options['account_page'] ); ?>" class="dashboard-cards__link">Mitgliedschaft verwalten</a>
 		</li>
 	</ul>
-	<?php
+					<?php
 }
 
-/**
- * Show support related items.
- */
-function show_support() {
-	?>
-	<h2 class="support__heading">Hilfe & Support</h2>
-	<ul class="support__list">
-		<li class="support__item"><a href="<?php echo esc_url( get_permalink( get_page_by_path( 'faqs' ) ) ); ?>">Zu den häufigen Fragen</a></li>
-		<li class="support__item"><a href="<?php echo esc_url( get_permalink( get_page_by_path( 'kontaktformular' ) ) ); ?>">Uns jetzt kontaktieren</a></li>
-	</ul>
-	<?php
-}
-
-// Start the engine.
-genesis();
+				// Start the engine.
+				genesis();
