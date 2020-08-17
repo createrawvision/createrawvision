e4926d859338dbff6020f287a29a8338ec7040cf
Josef Wittmann <josefwittmann42@gmail.com>
2020-08-17 11:08:11 +0200

    Show popular recipes in grid

--- public_html/wp-content/themes/daily-dish-pro/page-beliebte-rezepte.php
+++ public_html/wp-content/themes/daily-dish-pro/page-beliebte-rezepte.php
@@ -1,11 +1,37 @@
 <?php
 
-add_action(
-	'genesis_entry_content',
-	function() {
-		global $wpb;
-		echo wp_kses_post( $wpb->top_bookmarks() );
-	}
-);
+add_action( 'genesis_entry_content', 'crv_top_bookmarks' );
 
+/**
+ * Shows the top bookmarks from wp-bookmarks in the same style as archives.
+ */
+function crv_top_bookmarks() {
+
+	$recipes_cat_id = 5869;
+
+	$post_ids = get_posts(
+		array(
+			'posts_per_page' => 24,
+			'fields'         => 'ids',
+			'meta_key'       => '_wpb_post_bookmark_count',
+			'order'          => 'DESC',
+			'orderby'        => 'meta_value_num',
+			'cat'            => $recipes_cat_id,
+		)
+	);
+
+	echo '<div class="crv-grid">';
+
+	foreach ( $post_ids as $post_id ) {
+		$title    = get_the_title( $post_id );
+		$link     = get_permalink( $post_id );
+		$image_id = get_post_thumbnail_id( $post_id );
+
+		require CHILD_DIR . '/templates/grid.php';
+	}
+
+	echo '</div>';
+}
+
+// Start the engine.
 genesis();
