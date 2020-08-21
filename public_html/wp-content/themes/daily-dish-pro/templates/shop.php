cd3e7da2421a7255940f1a6437d05454cdccb7a5
Josef Wittmann <josefwittmann42@gmail.com>
2020-08-21 06:45:11 +0200

    Add shop template

---
+++ public_html/wp-content/themes/daily-dish-pro/templates/shop.php
@@ -0,0 +1,45 @@
+<div class="crv-shop">
+<?php
+// All shop terms.
+$shop_terms = get_terms( array( 'taxonomy' => 'crv_shop_category' ) );
+
+foreach ( $shop_terms as $shop_term ) :
+
+	echo '<h2 class="crv-shop__term-name">' . esc_html( $shop_term->name ) . '</h2>';
+	echo '<div class="crv-shop__item-list">';
+
+	// All shop items for the term.
+	$query = new WP_Query(
+		array(
+			'nopaging'  => true,
+			'post_type' => 'crv_shop_item',
+			'tax_query' => array(
+				array(
+					'taxonomy' => 'crv_shop_category',
+					'fields'   => 'term_id',
+					'terms'    => $shop_term->term_id,
+				),
+			),
+		)
+	);
+
+	while ( $query->have_posts() ) :
+		$query->the_post();
+		?> 
+		<div class="crv-shop__item">
+			<?php the_post_thumbnail( array( 300, 300 ), array( 'class' => 'crv-shop__item-image' ) ); ?>
+			<h3 class="crv-shop__item-title"><?php the_title(); ?></h3>
+			<a href="<?php the_field( 'product_url' ); ?>">
+				<button class="crv-shop__item-button"><?php the_field( 'button_text' ); ?></button>
+			</a>
+		</div>
+		<?php
+	endwhile;
+
+	wp_reset_postdata();
+
+	echo '</div>';
+
+endforeach;
+?>
+</div>
