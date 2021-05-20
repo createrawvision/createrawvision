454eb302785767c47687adc7e7358c9a9e3d9b4c
Josef Wittmann <josef.wittmann@tutanota.com>
2021-05-20 20:53:33 +0200

    Adjust logo margin

--- public_html/wp-content/themes/daily-dish-pro/templates/keimling-recipe-link-logo.php
+++ public_html/wp-content/themes/daily-dish-pro/templates/keimling-recipe-link-logo.php
@@ -1,6 +1,6 @@
 <template id="crv-keimling-recipe-link-logo">
 	<img
-		style="height: 1.3em; margin-left: 0.1em;"
+		style="height: 1.3em; margin: 0 0.1em;"
 		src="<?php echo esc_url( CHILD_URL . '/images/keimling-logo.png' ); ?>"
 		class="crv-keimling-recipe-link-logo nopin"
 	/>
